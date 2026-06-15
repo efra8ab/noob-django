5aa63bb9aede25501ba3d3623dd68ae8c0b61346
efra8ab <efra8ab@gmail.com>
2026-06-14 21:18:12 -0600

    polls view and urls

--- noob/polls/views.py
+++ noob/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World!. You're at the polls")
+
+def results(request):
+    return HttpResponse('This is the response page!!!!')
+
+def detail(request, question_id):
+    return HttpResponse(f'you are looking at question {question_id} ')
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id} ')
 
 # Create your views here.
